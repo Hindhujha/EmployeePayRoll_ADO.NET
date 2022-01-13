6e4e826089fe1b9d75843ceff8dfedc0736a41df
unknown <hindhujhasridhar@gmail.com>
2022-01-13 21:35:13 +0530

    ADO.NET-EMPLOYEE PAYROLL PROBLEMS

---
+++ EmployeePayRoll/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+namespace EmployeePayRoll
+{
+    class Program
+    {
+         static void Main(string[] args)
+        
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE PAYROLL PROBLEMS ADO.NET");
+        }
+    }
+}
